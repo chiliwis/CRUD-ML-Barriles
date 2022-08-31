56a69dab71d4876049100ea6bb84dbdb2e66b69e
chiliwis <cristinasanchezg@gmail.com>
2022-08-30 21:13:19 -0500

    Inicio listado vacio

--- js/main.js
+++ js/main.js
@@ -1,1 +1,47 @@
-console.log("Entro al main.js")
+console.log("Entro al main.js");
+
+//Variables
+const formularioU = document.getElementById('formulario');
+const listaBarrilesU = document.getElementById('listaBarriles');
+let arrayBarriles = [];
+
+
+//Funciones
+
+const CrearBarril = (barril) => {
+
+    let item = {
+        barril: barril,
+        estado: false
+    }
+
+    arrayBarriles.push(item);
+    return item;
+
+}
+
+const GuardarDB = (barril) => {
+    localStorage.setItem('entregados', JSON.stringify(arrayBarriles));
+
+}
+
+const LeerDB = () => {
+    listaBarrilesU.innerHTML = '';
+}
+//Eventos
+
+formularioU.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+    let barrilU = document.querySelector('#barril').value;
+
+    CrearBarril(barrilU);
+
+    GuardarDB();
+
+    formularioU.reset();
+
+
+});
+
+document.addEventListener('DOMContentLoaded', LeerDB);
